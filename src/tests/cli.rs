1ca1f34fcf15ea8c7fc6ea8fb235dc8febdf1a3d
Igbiriki Francis <francis.igbiriki@gmail.com>
2025-03-09 14:31:53 +0100

    update definitions

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -12,7 +12,7 @@ fn check_answer_validity(){
 
 #[test]
 fn file_doesnt_exist()-> Result<(), Box<dyn std::error::Error>>{
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("clienv")?;
 
     cmd.arg("foobar").arg("test/file/doesnt/exist");
     cmd.assert().failure().stderr(prdicate::str::contains("could not read file"));
@@ -26,7 +26,7 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error:Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
     file.write_str("A test\nActual content\nMore content\nAnother test")?;
 
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("clienv")?;
     cmd.arg("test").arg(file.path());
     cmd.assert().success().stdout(predicate::str::contains("A test\nAnother test"));
 
@@ -40,7 +40,7 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error:Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
     file.write_str("A test\nActual content\nMore content\nAnother test")?;
 
-    let mut cmd = Command::cargo_bin("grrs")?;
+    let mut cmd = Command::cargo_bin("clienv")?;
     cmd.arg("test").arg(file.path());
     cmd.assert().success().stdout(predicate::str::contains(""));
 
